de8688add0372855333098542a472dddbe808d49
Geraldo Luiz <geraldobl58@gmail.com>
2023-01-26 16:53:04 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider as PaperProvider, Button } from "react-native-paper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <Button mode="contained" onPress={() => console.log("Click")}>
+          Click
+        </Button>
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,23 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@config': './src/config',
+            '@routes': './src/routes',
+            '@services': './src/services',
+            '@screens': './src/screens',
+            '@theme': './src/theme',
+            '@utils': './src/utils',
+          }
+        }
+      ]
+    ]
   };
 };
