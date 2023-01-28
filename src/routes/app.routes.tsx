110ee4a48d031533a1dfad223e30ea626d5e749a
Geraldo Luiz <geraldobl58@gmail.com>
2023-01-28 17:27:36 -0300

    feat: Configure route icon

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,10 +2,14 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 
+import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
+
 import { Home } from "@screens/Home";
 import { Favorites } from "@screens/Favorites";
 import { Cart } from "@screens/Cart";
 import { Profile } from "@screens/Profile";
+
+import { useTheme } from "styled-components/native";
 
 type AppRoutesProps = {
   home: undefined;
@@ -16,10 +20,41 @@ type AppRoutesProps = {
 
 const Tab = createMaterialBottomTabNavigator<AppRoutesProps>();
 
+function setIcon(route: any, routeStatus: any) {
+  let iconName = "";
+
+  switch (route.name) {
+    case "home":
+      iconName = "home";
+      break;
+    case "favorites":
+      iconName = "heart";
+      break;
+    case "cart":
+      iconName = "shopping-cart";
+      break;
+    case "profile":
+      iconName = "user";
+      break;
+    default:
+      break;
+  }
+
+  return <FontAwesomeIcon name={iconName} style={{ fontSize: 20 }} />;
+}
+
 export function AppRoutes() {
+  const { COLORS } = useTheme();
+
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: (routeStatus) => {
+            return setIcon(route, routeStatus);
+          },
+        })}
+      >
         <Tab.Screen name="home" component={Home} options={{ title: "Home" }} />
         <Tab.Screen
           name="favorites"
